e282257dbe8337dfefa223c859cca24f4ca25df4
Eric Marty <contact@ericmarty.co>
2017-11-24 13:15:38 -0800

    Animate dialog characters

--- Assets/DialogManager.cs
+++ Assets/DialogManager.cs
@@ -43,7 +43,23 @@ public class DialogManager : MonoBehaviour {
 
 		string sentence = sentences.Dequeue();
 
-		dialogText.text = sentence;
+		// Make sure we stop animating current dialog before we start animating new one.
+		// If typeSentence is already running we will stop doing so
+		// so we can start a new one.
+		StopAllCoroutines();
+
+		StartCoroutine(TypeSentence(sentence));
+	}
+
+	IEnumerator TypeSentence(string sentence)
+	{
+		dialogText.text = "";
+		foreach(char letter in sentence.ToCharArray())
+		{
+			dialogText.text += letter;
+			// Wait a single frame
+			yield return null;
+		}
 	}
 
 	void EndDialog()
