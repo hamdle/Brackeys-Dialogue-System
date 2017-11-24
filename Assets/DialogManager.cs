410b1cbf816134caef97283010f76c0ed1ca5ceb
Eric Marty <contact@ericmarty.co>
2017-11-24 12:34:34 -0800

    Dialog system built and animation hooked up

---
+++ Assets/DialogManager.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DialogManager : MonoBehaviour {
+
+	public Text nameText;
+	public Text dialogText;
+
+	public Animator animator;
+
+	private Queue<string> sentences;
+
+	// Use this for initialization
+	void Start () {
+		sentences = new Queue<string>();
+	}
+
+	public void StartDialog(Dialog dialog)
+	{
+		animator.SetBool("IsOpen", true);
+
+		nameText.text = dialog.name;
+
+		sentences.Clear();
+
+		foreach(string sentence in dialog.sentences)
+		{
+			sentences.Enqueue(sentence);
+		}
+
+		DisplayNextSentence();
+	}
+
+	public void DisplayNextSentence()
+	{
+		if (sentences.Count == 0)
+		{
+			EndDialog();
+			return;
+		}
+
+		string sentence = sentences.Dequeue();
+
+		dialogText.text = sentence;
+	}
+
+	void EndDialog()
+	{
+		animator.SetBool("IsOpen", false);
+	}
+
+
+}
